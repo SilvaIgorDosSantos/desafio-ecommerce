6d0d5c86943a81b902cebbb1132374743f627ce6
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-03-31 18:56:51 -0300

    Create schema files

---
+++ desafio/schema.py
@@ -0,0 +1,16 @@
+import graphene
+import graphql_jwt
+
+import ecommerce.schema
+
+
+class Query(ecommerce.schema.Query, graphene.ObjectType):
+    pass
+
+class Mutation(ecommerce.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ ecommerce/schema.py
@@ -0,0 +1,10 @@
+import graphene
+
+from graphene_django import DjangoObjectType
+
+
+class Query(graphene.ObjectType):
+    pass
+
+class Mutation(graphene.ObjectType):
+    pass

58bf72b90e81789d965922d401cdad77aa3c92d9
SilvaIgorDosSantos <silvaigordossantos@gmail.com>
2021-03-31 19:59:34 -0300

    Update settings to differentiate SECRET_KEY from development and production

--- desafio/settings.py
+++ desafio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', '=(6u&j*p_o_#tr2o-a&y)h)idc#3bs7hh#ry&xfj$ucu2(k%iv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
